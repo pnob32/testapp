68221222eeaac5ff262884dfcf8b270eb7b0ac59
Ubuntu <ubuntu@ip-172-31-39-176.us-west-2.compute.internal>
2016-03-04 06:34:39 +0000

    backend changes to flask

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__, static_url_path='')
+#app.config.from_object('config')
+
+#api = restful.Api(app)
 
 users = [
     {
-        'first_name': u'Patrick',
-        'last_name': u'Noble',
+        'firstname': u'Patrick',
+        'lastname': u'Noble',
         'age': 24 
     },
     {
-        'first_name': u'Robyn',
-        'last_name': u'Cromwell',
+        'firstname': u'Robyn',
+        'lastname': u'Cromwell',
         'age': 28 
     }
 ]
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Acess-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 @app.route('/user/<user_id>')
 def get_user(user_id):
@@ -29,6 +39,11 @@ def get_user(user_id):
 
 @app.route('/users', methods=['GET'])
 def get_users():
+    return json.dumps(users)
+
+@app.route('/users', methods=['POST'])
+def post_users():
+    users.append(request.json)
     return json.dumps(users)
 
 @app.route('/helloworld', methods=['GET'])
